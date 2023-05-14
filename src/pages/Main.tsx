b650adbd41f21cea23a491c5886d78576ae72883
Ain Chung <artnouveau0804@gmail.com>
2023-05-14 18:43:25 +0900

    feat:  데이터 받아오기 코드

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../components/base/Header';
 import InputTodo from '../components/todo/InputTodo';
-import { TodoDataType } from '../types/types';
 import TodoList from '../components/todo/TodoList';
+import { TodoDataType } from '../types/types';
+import { getTodoList } from '../api/todo';
 
 const Main = () => {
   const [todoListData, setTodoListData] = useState<TodoDataType[] | []>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await getTodoList();
+      setTodoListData(data || []);
+    })();
+  }, []);
 
   return (
     <div className="container">
