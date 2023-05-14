0c1a0bef62f7a4b6f1cd8ca0ade97bcd0669e8ed
Ain Chung <artnouveau0804@gmail.com>
2023-05-14 17:38:19 +0900

    feat: Main & Header.tsx 및 CSS 재구성
    Main에 있는 태그의 css는 App.css에 지정.
    CSS 파일은 컴포넌트 별로 나누는게 깨끗해 보여서 .tsx파일과 .css 파일 같이 묶음.

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header_title">Toodos</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/base/Header';
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="inner">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
