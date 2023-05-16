9cfe927fb146ab5671e165e8374a8b7fe1e67a25
rabbit-22 <chlwlal20@gmail.com>
2023-05-16 16:59:50 +0900

    style: input 스타일 추가

--- src/components/todo/InputTodo.tsx
+++ src/components/todo/InputTodo.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Todo.css';
-
 import useFocus from '../../hooks/useFocus';
 import { SetStateType, TodoDataType } from '../../types/types';
 import ItemButton from './ItemButton';
-
-import Dropdown from './Dropdown';
+import Dropdown from '../dropdown/Dropdown';
 import Spinner from '../base/Spinner';
 import useFetchTodo from '../../hooks/useFetchTodo';
 
@@ -17,12 +15,16 @@ const InputTodo = ({ setTodos }: InputTodoProps) => {
   const { ref, setFocus } = useFocus();
   const { isLoading, inputText, setInputText, handleChange, handleSubmit } =
     useFetchTodo(setTodos);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
   useEffect(setFocus, [setFocus]);
 
   return (
     <>
       <form className="form-container" onSubmit={handleSubmit}>
-        <div className="form-box">
+        <div
+          className={'form-box' + (isLoading || isTyping ? ' progress' : '')}
+        >
+          <ItemButton mode="search" />
           <input
             className="input-text"
             placeholder="Add new todo..."
@@ -30,6 +32,8 @@ const InputTodo = ({ setTodos }: InputTodoProps) => {
             value={inputText}
             onChange={handleChange}
             disabled={isLoading}
+            onKeyDown={() => setIsTyping(true)}
+            onKeyUp={() => setIsTyping(false)}
           />
           {!isLoading ? <ItemButton mode="add" /> : <Spinner />}
         </div>
